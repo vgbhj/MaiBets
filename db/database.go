80e6dd04e8ff878e6902b372f60c771604d60a6d
vgbhj <mishaslsk@gmail.com>
2024-12-13 22:49:45 +0300

    Add docker-compose

--- db/database.go
+++ db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,9 +11,9 @@ import (
 // ConnectDB устанавливает соединение с базой данных
 func ConnectDB() *sql.DB {
 	// Open the connection
-	// db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
-	connStr := "host=localhost user=myuser password=mypassword dbname=mydb port=5432 sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", os.Getenv("DB_URL"))
+	// connStr := "host=0.0.0.0 user=user password=password dbname=mydatabase port=5432 sslmode=disable"
+	// db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		panic(err)
